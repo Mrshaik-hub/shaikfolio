dc0ce66b4648743c426316f0a5985c4d3d01e47c
Codewithshaik <imoxygod@gmail.com>
2025-07-30 03:28:36 +0530

    changes latest

---
+++ connectdb.php
@@ -0,0 +1,13 @@
+<?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "shaikfolio";  // ✅ Use your actual database name here
+//
+//$conn = new mysqli($servername, $username, $password, $dbname);
+//
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
+//?>
